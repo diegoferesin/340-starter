04c4dee8637bd0b2996f1c8c2d62bb95dd7c95c3
Diego Feresin <diegoferesin@gmail.com>
2025-07-24 23:27:32 -0300

    w4 - Account: Process Registration

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -10,4 +10,7 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Route to build registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
+// Route to process registration
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
 module.exports = router 
