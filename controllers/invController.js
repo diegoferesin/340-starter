a0fc5459f9ff91f7d29c5bc2ff07bd13f07f3d8c
Diego Feresin <diegoferesin@gmail.com>
2025-07-31 23:36:03 -0300

    w05 - Select Inventory Items

--- controllers/invController.js
+++ controllers/invController.js
@@ -24,9 +24,13 @@ invCont.buildByClassificationId = async function (req, res, next) {
  * ************************** */
 invCont.buildManagement = async function (req, res, next) {
   let nav = await utilities.getNav()
+  
+  const classificationSelect = await utilities.buildClassificationList()
+  
   res.render("./inventory/management", {
     title: "Inventory Management",
     nav,
+    classificationSelect,
   })
 }
 
@@ -162,5 +166,17 @@ invCont.buildByInventoryId = async function (req, res, next) {
   })
 }
 
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
+  }
+}
 
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities/")
 const regValidate = require('../utilities/account-validation')
 
 // Route to build inventory by classification view
@@ -14,6 +15,9 @@ router.get("/", invController.buildManagement)
 router.get("/add-classification", invController.buildAddClassification)
 // Route to build add-inventory view
 router.get("/add-inventory", invController.buildAddInventory)
+
+// Route to get inventory data as JSON
+router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON))
 
 // Route to process add-classification form
 router.post(
